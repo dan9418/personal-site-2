7cf6fb1384a0eb32323d0a9d80be48dc9c906ea5
Dan <dpb34@pitt.edu>
2019-07-29 15:38:01 -0400

    page positioning

--- src/Main/Resume/Resume.tsx
+++ src/Main/Resume/Resume.tsx
@@ -40,6 +40,24 @@ export class ResumePosition extends React.Component<any, any> {
 	};
 }
 
+export class ResumeDetail extends React.Component<any, any> {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render = () => {
+		return (
+			<div className='resume-detail'>
+				{this.props.children}
+				<div className='resume-detail-date'>
+					{this.props.date}
+				</div>
+			</div>
+		);
+	};
+}
+
 export class Resume extends React.Component<any, any> {
 
 	constructor(props) {
@@ -50,30 +68,48 @@ export class Resume extends React.Component<any, any> {
 		return (
 			<div className='resume-container'>
 				<ResumeSection title='Education' />
-					<ResumePosition
+				<ResumePosition
 					company='University of Pittsburgh, Swanson School of Engineering'
 					date='April 2017'
 					location='Pittsburgh, PA'
-					></ResumePosition>
+				>
+					<ResumeDetail>GPA: 3.71</ResumeDetail>
+					<ResumeDetail>Major: BS, Computer Engineering</ResumeDetail>
+					<ResumeDetail>Special Distictions: Outstanding Senior in Department</ResumeDetail>
+				</ResumePosition>
 				<ResumeSection title='Experience' />
 				<ResumePosition
 					company='Bentley Systems'
 					title='Associate Software Engineer'
 					date='May 2016 - July 019'
 					location='Pittsburgh, PA'
-					></ResumePosition>
-					<ResumePosition
+				>
+					<ResumeDetail>Selected for inaugural international program (3 months in Vilnius, Lithuania)</ResumeDetail>
+					<ResumeDetail>Works in cooperative agile environments using Scrum and Kanban</ResumeDetail>
+					<ResumeDetail>Uses .NET, TypeScript, AngularJS, C++, and SQL in web apps and APIs</ResumeDetail>
+					<ResumeDetail>Develops enhancements, fixes defects, and performs research-oriented tasks</ResumeDetail>
+					<ResumeDetail>Writes integration and unit tests using several frameworks</ResumeDetail>
+				</ResumePosition>
+				<ResumePosition
 					company='The Bank of New York Mellon Corporation'
 					title='Software Engineer Intern'
 					date='May 2015 – August 2015'
 					location='Pittsburgh, PA'
-					></ResumePosition>
-					<ResumePosition
+				>
+					<ResumeDetail>Automated daily data comparisons for updating central Reference Data Hub</ResumeDetail>
+					<ResumeDetail>Implemented in Java for separate partitions of MVS/Linux mainframe</ResumeDetail>
+					<ResumeDetail>Mapped data across DB2 databases to unify information</ResumeDetail>
+				</ResumePosition>
+				<ResumePosition
 					company='University of Pittsburgh, Residence Life'
 					title='Resident Assistant'
 					date='August 2014 - December 2015'
 					location='Pittsburgh, PA'
-					></ResumePosition>
+				>
+					<ResumeDetail>Enforced community standards and expectations</ResumeDetail>
+					<ResumeDetail>Managed 57 freshmen residents</ResumeDetail>
+					<ResumeDetail>Organized social and engineering programs for students</ResumeDetail>
+				</ResumePosition>
 				<ResumeSection title='Skills' />
 				<ResumeSection title='Awards' />
 				<ResumeSection title='Activities' />
