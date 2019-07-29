302897559d60ff6dd9ff7a500a79254f4cc66520
Dan <dpb34@pitt.edu>
2019-07-29 14:56:27 -0400

    resume section templates

--- src/Main/Resume/Resume.tsx
+++ src/Main/Resume/Resume.tsx
@@ -17,6 +17,29 @@ export class ResumeSection extends React.Component<any, any> {
 	};
 }
 
+export class ResumePosition extends React.Component<any, any> {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render = () => {
+		return (
+			<div className='resume-position'>
+				<div className='resume-position-header'>
+					<div className='resume-position-header-company'>{this.props.company}</div>
+					<div className='resume-position-header-title'>{this.props.title}</div>
+					<div className='resume-position-subheader'>
+						<div className='resume-position-header-location'>{this.props.location}</div>
+						<div className='resume-position-header-date'>{this.props.date}</div>
+					</div>
+				</div>
+				<div className='resume-position-content'>{this.props.children}</div>
+			</div>
+		);
+	};
+}
+
 export class Resume extends React.Component<any, any> {
 
 	constructor(props) {
@@ -26,11 +49,34 @@ export class Resume extends React.Component<any, any> {
 	render = () => {
 		return (
 			<div className='resume-container'>
-				<ResumeSection title='Education'/>
-				<ResumeSection title='Experience'/>
-				<ResumeSection title='Skills'/>
-				<ResumeSection title='Awards'/>
-				<ResumeSection title='Activities'/>
+				<ResumeSection title='Education' />
+					<ResumePosition
+					company='University of Pittsburgh, Swanson School of Engineering'
+					date='April 2017'
+					location='Pittsburgh, PA'
+					></ResumePosition>
+				<ResumeSection title='Experience' />
+				<ResumePosition
+					company='Bentley Systems'
+					title='Associate Software Engineer'
+					date='May 2016 - July 019'
+					location='Pittsburgh, PA'
+					></ResumePosition>
+					<ResumePosition
+					company='The Bank of New York Mellon Corporation'
+					title='Software Engineer Intern'
+					date='May 2015 – August 2015'
+					location='Pittsburgh, PA'
+					></ResumePosition>
+					<ResumePosition
+					company='University of Pittsburgh, Residence Life'
+					title='Resident Assistant'
+					date='August 2014 - December 2015'
+					location='Pittsburgh, PA'
+					></ResumePosition>
+				<ResumeSection title='Skills' />
+				<ResumeSection title='Awards' />
+				<ResumeSection title='Activities' />
 			</div>
 		);
 	};
