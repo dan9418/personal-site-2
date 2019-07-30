a6d7a7ab936f81376b8c7b48bc67b5bf6fdee3ac
Dan <dpb34@pitt.edu>
2019-07-30 16:46:04 -0400

    redo icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./App.css";
 import { MenuButton } from "./Menu/MenuButton";
-import { Icon } from "./Icon/Icon";
 import { Bio } from "./Main/Bio/Bio";
 import { Resume } from "./Main/Resume/Resume";
 import { WebDev } from "./Main/WebDev/WebDev";
 import { Music } from "./Main/Music/Music";
 import { Art } from "./Main/Art/Art";
 import { SidePanelLink } from "./SidePanel/SidePanelLink";
+import { ICONS } from "./Common/Icon";
 
 export class App extends React.Component<any, any> {
 
     constructor(props) {
         super(props);
         this.state = {
-            page: Art
+            page: Bio
         }
     }
 
@@ -29,18 +29,18 @@ export class App extends React.Component<any, any> {
         let Page = this.state.page;
         return (
             <>
-                <div id="top">
-                    <MenuButton text='Bio' setPage={this.setPage} page={Bio} icon={<Icon path='./img/icons/home.svg' />} />
-                    <MenuButton text='Resume' setPage={this.setPage} page={Resume} icon={<Icon path='./img/icons/resume.svg' />} />
-                    <MenuButton text='Web Dev' setPage={this.setPage} page={WebDev} icon={<Icon path='./img/icons/webdev.svg' />} />
-                    <MenuButton text='Music' setPage={this.setPage} page={Music} icon={<Icon path='./img/icons/music.svg' />} />
-                    <MenuButton text='Art' setPage={this.setPage} page={Art} icon={<Icon path='./img/icons/art.svg' />} />
+                <div className="navbar">
+                    <MenuButton text='Bio' setPage={this.setPage} page={Bio} icon={ICONS.home} />
+                    <MenuButton text='Resume' setPage={this.setPage} page={Resume} icon={ICONS.resume} />
+                    <MenuButton text='Web Dev' setPage={this.setPage} page={WebDev} icon={ICONS.webdev} />
+                    <MenuButton text='Music' setPage={this.setPage} page={Music} icon={ICONS.music} />
+                    <MenuButton text='Art' setPage={this.setPage} page={Art} icon={ICONS.art} />
                 </div>
-                <div id='left'>
-                    <div id='side-panel-header'>
-                        <img id='me' src='./img/me.jpg' />
+                <div className='side-panel'>
+                    <div className='side-panel-header'>
+                        <img className='me' src='./img/me.jpg' />
                     </div>
-                    <div id='side-panel-info'>
+                    <div className='side-panel-info'>
                         <div className='side-panel-info-item name'>
                             Dan Bednarczyk
                         </div>
@@ -48,15 +48,15 @@ export class App extends React.Component<any, any> {
                             Web Developer
                         </div>
                         <div className='side-panel-info-item location'>
-                            <img className='info-icon' src='./img/icons/location.svg' />
+                            {ICONS.location}
                             Davis, CA
                         </div>
                     </div>
-                    <SidePanelLink text='LinkedIn' icon={<Icon path='./img/icons/linkedin.svg' />} />
-                    <SidePanelLink text='GitHub' icon={<Icon path='./img/icons/github.svg' />} />
-                    <SidePanelLink text='Bandcamp' icon={<Icon path='./img/icons/bandcamp.svg' />} />
+                    <SidePanelLink text='LinkedIn' icon={ICONS.linkedin} />
+                    <SidePanelLink text='GitHub' icon={ICONS.github} />
+                    <SidePanelLink text='Bandcamp' icon={ICONS.bandcamp} />
                 </div>
-                <div id='right'>
+                <div className='main'>
                     <Page />
                 </div>
             </>
