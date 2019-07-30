2e0498f0f68672ea50904c4baf6591b37f181b90
Dan <dpb34@pitt.edu>
2019-07-30 08:38:58 -0400

    more content

--- src/Main/Art/Art.tsx
+++ src/Main/Art/Art.tsx
@@ -9,9 +9,10 @@ export class Art extends React.Component<any, any> {
 
 	render = () => {
 		return (
-				<div className='page-container'>
-					
-				</div>
+			<div className='page-container'>
+				<h1>Art</h1>
+				<p>I love art in its many forms - I enjoy drawing and painting, but have mostly been drawn to graphic design. I have done lots of artwork over the years, including work for universities, clubs, and all of my musical projects. My galleries are available below.</p>
+			</div>
 		);
 	};
 }
